36b6365ce960bf4e05a4c6f7a4de164f7e661c06
angelsc75 <asc7es@gmail.com>
2024-07-24 10:48:46 +0200

    actualización de gui
    hay que afinar mucho la visualización de datos

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Configura tu conexión a la base de datos
-DATABASE_URL = 'mysql+pymysql://root:admin@localhost/scraping_quotes'
+#DATABASE_URL = 'mysql+pymysql://root:admin@localhost/scraping_quotes'
+load_dotenv()  # Carga las variables del archivo .env
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 
 # Configuración de la página
@@ -30,3 +37,25 @@ st.dataframe(data)
 # Mostrar gráficos básicos
 st.write("### Gráficos Básicos")
 st.bar_chart(data['id'])
+
+
+# Mostrar opciones
+option = st.selectbox('Selecciona una opción:', ['Opción 1', 'Opción 2'])
+
+# mostrar gráficos avanzados
+
+fig, ax = plt.subplots()
+sns.histplot(data['some_column'], ax=ax)
+st.pyplot(fig)
+
+# interactividad
+
+if st.button('Mostrar Mensaje'):
+    st.write('¡Botón presionado!')
+# layouts y diseños
+
+col1, col2 = st.columns(2)
+with col1:
+    st.write("Columna 1")
+with col2:
+    st.write("Columna 2")
