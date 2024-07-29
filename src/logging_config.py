7e639d3fc69b66950117885e90f6821e789728cc
angelsc75 <asc7es@gmail.com>
2024-07-29 09:11:07 +0200

    Update logging_config.py

--- src/logging_config.py
+++ src/logging_config.py
@@ -1,38 +1,34 @@
 import logging
 
-"""
-Configura y devuelve un logger para la aplicación.
-
-Este logger registra mensajes en un archivo y en la consola con diferentes niveles de severidad y formatos.
-
-Returns:
-    logger (logging.Logger): El logger configurado.
-"""
-
 def setup_logger():
-    # Crear un logger con el nombre 'loggs_scrap'
+    """
+    Configura y devuelve un logger para la aplicación.
+
+    Este logger registra mensajes en un archivo y en la consola con diferentes niveles de severidad y formatos.
+
+    Returns:
+        logger (logging.Logger): El logger configurado.
+    """
     logger = logging.getLogger('loggs_scrap')
-    # Configurar el nivel de severidad del logger a DEBUG
     logger.setLevel(logging.DEBUG)
 
-    # Crear un manejador de archivo que registra mensajes en 'loggs_scrap.log' con codificación UTF-8
-    fh = logging.FileHandler('loggs_scrap', encoding='utf-8')
-    # Configurar el nivel de severidad del manejador de archivo a DEBUG
-    fh.setLevel(logging.DEBUG)
+    # Evita agregar manejadores múltiples
+    if not logger.hasHandlers():
+        # Crear un manejador de archivo que registra mensajes en 'loggs_scrap.log' con codificación UTF-8
+        fh = logging.FileHandler('loggs_scrap.log', encoding='utf-8')
+        fh.setLevel(logging.DEBUG)
+        
+        # Crear un manejador de consola que imprime mensajes de nivel ERROR y superior
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)
+        
+        # Definir el formato de los mensajes del log
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        
+        # Agregar los manejadores (archivo y consola) al logger
+        logger.addHandler(fh)
+        logger.addHandler(ch)
     
-    # Crear un manejador de consola que imprime mensajes de nivel ERROR y superior
-    ch = logging.StreamHandler()
-    # Configurar el nivel de severidad del manejador de consola a ERROR
-    ch.setLevel(logging.ERROR)
-    
-    # Definir el formato de los mensajes del log
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    # Asignar el formato definido al manejador de archivo
-    fh.setFormatter(formatter)
-    
-    # Agregar los manejadores (archivo y consola) al logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-
-    # Devolver el logger configurado
     return logger
